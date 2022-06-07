9cd94596cbaa6b38a0554871320a19cc77dd6aba
Manjunath HM <manjunath@altimetrik.com>
2022-06-07 13:33:10 +0530

    Added Id Generation Util

---
+++ batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/constants/BattingServiceConstants.java
@@ -0,0 +1,6 @@
+package org.fourstack.reactivecricinfo.battinginfoservice.constants;
+
+public class BattingServiceConstants {
+    public static final String BATTING_INFO_ID_PREFIX = "BAT";
+    public static final String HYPHEN = "-";
+}
