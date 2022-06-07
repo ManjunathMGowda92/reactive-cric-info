a02c308158bc27fe392182ef1a84761270d9b246
Manjunath HM <manjunath@altimetrik.com>
2022-06-07 16:11:58 +0530

    Audit Aware for DAO models

--- batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/BattingInfoServiceApplication.java
+++ batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/BattingInfoServiceApplication.java
@@ -2,8 +2,10 @@ package org.fourstack.reactivecricinfo.battinginfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
+@EnableReactiveMongoAuditing
 public class BattingInfoServiceApplication {
 
 	public static void main(String[] args) {
