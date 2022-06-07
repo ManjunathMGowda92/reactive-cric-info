561d42bb0c780655e6cd892e5b394ddb053a09ff
Manjunath HM <manjunath@altimetrik.com>
2022-06-07 13:37:45 +0530

    Added Controller classes

---
+++ batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/controller/BattingInfoCommandController.java
@@ -0,0 +1,8 @@
+package org.fourstack.reactivecricinfo.battinginfoservice.controller;
+
+/**
+ * RestController class which acts for the commands POST, PUT, DELETE and PATCH.
+ * It exposes the methods which are responsible for the change of state of Object.
+ */
+public class BattingInfoCommandController {
+}
---
+++ batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/controller/BattingInfoQueryController.java
@@ -0,0 +1,9 @@
+package org.fourstack.reactivecricinfo.battinginfoservice.controller;
+
+/**
+ * RestController class which acts for query methods (GET..)
+ * It is responsible to expose only query related methods,
+ * which doesn't change state of Object.
+ */
+public class BattingInfoQueryController {
+}
