f07ad5d31d2fe539ff18ff425b32ce2445bdafaf
Manjunath HM <manjunath@altimetrik.com>
2022-06-20 15:23:14 +0530

    Added test case for batting-service

--- batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/dao/BattingInfoDao.java
+++ batting-info-service/src/main/java/org/fourstack/reactivecricinfo/battinginfoservice/dao/BattingInfoDao.java
@@ -2,8 +2,10 @@ package org.fourstack.reactivecricinfo.battinginfoservice.dao;
 
 import org.fourstack.reactivecricinfo.battinginfoservice.model.BattingInfo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface BattingInfoDao extends ReactiveMongoRepository<BattingInfo, String> {
 
     Mono<BattingInfo> findByPlayerId(String playerId);
